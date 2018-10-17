c58f030b6bab20203baba03b40b20ae8e1494d6d
Shameem Reza <codechef@yahoo.com>
2018-10-17 23:50:51 +0600

    Update pwreset.php

--- source/pwreset.php
+++ source/pwreset.php
@@ -6,6 +6,7 @@ include("includes/controller.php");
     <head>
         <title>PHP User Management - Password Reset</title>
         <meta charset="UTF-8">
+        <meta name="author" content="Shameem Reza">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link href="css/bootstrap.min.css" rel="stylesheet">
