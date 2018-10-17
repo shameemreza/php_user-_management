1caf4efa3c5baf468243efc282685bddd17f6ce2
Shameem Reza <codechef@yahoo.com>
2018-10-17 23:49:07 +0600

    Update login.php

--- source/login.php
+++ source/login.php
@@ -11,6 +11,7 @@ $form = new Form;
     <head>
         <title>PHP User Management - Shameem</title>
         <meta charset="UTF-8">
+        <meta name="author" content="Shameem Reza">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link href="css/bootstrap.min.css" rel="stylesheet">
