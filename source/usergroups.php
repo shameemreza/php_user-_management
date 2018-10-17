dd6f1e5f503aec16eda0e0ed757fdebd92bcd8b2
Shameem Reza <codechef@yahoo.com>
2018-10-17 23:58:52 +0600

    Update usergroups.php

--- source/usergroups.php
+++ source/usergroups.php
@@ -13,6 +13,7 @@ else{
     <head>
         <title>PHP User Management - Shameem</title>
         <meta charset="UTF-8">
+        <meta name="author" content="Shameem Reza">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link href="css/bootstrap.min.css" rel="stylesheet">
