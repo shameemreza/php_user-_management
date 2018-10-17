0ab932a9117b46c2746141187f711e253169780f
Shameem Reza <codechef@yahoo.com>
2018-10-17 23:59:22 +0600

    Update index.php

--- source/install/index.php
+++ source/install/index.php
@@ -3,6 +3,7 @@
     <head>
         <title>PHP User Management - Shameem</title>
         <meta charset="UTF-8">
+        <meta name="author" content="Shameem Reza">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/ico" href="favicon.ico">
 
